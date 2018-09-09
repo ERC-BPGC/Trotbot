0ae5a06ad2f49617f51b5acf7c0f0e08b9286f29
Harshal <hardesh1deshpande@gmail.com>
2018-09-09 23:50:13 +0530

    Added navigation folder

---
+++ navigation/dijkstra.py
@@ -0,0 +1,41 @@
+graph = {'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
+ 
+def dijkstra(graph,start,goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph
+    infinity = 9999999
+    path = []
+    for node in unseenNodes:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+ 
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+ 
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.pop(minNode)
+ 
+    currentNode = goal
+    while currentNode != start:
+        try:
+            path.insert(0,currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print('Path not reachable')
+            break
+    path.insert(0,start)
+    if shortest_distance[goal] != infinity:
+        print('Shortest distance is ' + str(shortest_distance[goal]))
+        print('And the path is ' + str(path))
+ 
+ 
+dijkstra(graph, 'a', 'b')
