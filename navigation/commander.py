5bfb39c6368b63934aa55061202a28f9f57a580f
Atharv Sonwane <atharvs.twm@gmail.com>
2018-11-23 04:03:43 +0530

    Add files via upload

--- navigation/commander.py
+++ navigation/commander.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
+__author__ = "Atharv"
 
-__author__ = 'Atharb'
 import rospy
+import dynamic_rrt_integration as dri
 from obstacle_expander.msg import Ipoly
+
 
 class Current():
     """
@@ -13,38 +15,40 @@ class Current():
         self.initialize_data()
 
     def initialize_data(self):
-        self.curr_pos = [0, 0]
+        """Initialize data."""
+        self.curr_pos = (0, 0)
         self.obstacle_list = []
         self.path = []
-        self.goal_pos = []
-        self.old_data = [0, 0]
+        self.goal_pos = (0, 0)
+        self.curr_target = (0, 0)
         self.target_changed = False
 
     def odom_update(self, data):
         """Update current position of bot."""
-        self, curr_pos[0] = data.pose.pose.position.x
-        self.curr_pos[1] = data.pose.pose.position.y
-
+        self, curr_pos = (data.pose.pose.position.x, data.pose.pose.position.y)
 
     def main_response(self, data):
        """Updates goal position and calls rrt."""
-        if([data.x, data.y] != self.curr_target):
-            self.curr_target = [data.x, data.y]
-            path = do_RRT(show_animation = False, start_points_coors = [0, 0] , end_point_coors = self.curr_target , self.obstacle_list)
-            target_changed = True
+        if((data.x, data.y) != self.curr_target):
+            self.curr_target = (data.x, data.y)
+            path = do_RRT(show_animation = False, start_points_coors = (0, 0), end_point_coors = self.curr_target, self.obstacle_list)
+            self.target_changed = True
         else:
-            target_changed = False
+            self.target_changed = False
 
+    
     def update_obst_list(self, data):
-        """Create obstacle list"""
-        tobi=[]
+        """Create obstacle list."""
         self.obstacle_list = []
         for i in data.eternal_bliss:
-            for j in i.bliss:
-                tobi.append((j.x,j.y))
-            poly=Polygon(tobi)
-            self.obstacle_list.append(poly)
-            tobi=[]
+            points_list = [(j.x, j.y) for j in i.bliss]
+            self.obstacle_list.append(Polygon(points_list))
+
+
+    def dynamic_caller(self):
+        """Call dynamic checker while en route"""
+        if not self.target_changed:
+            self.path = self.dynamic_rrt(start = self.curr_pos, end = self.curr_target, path = self.curr_path, obstacle_list = self.obstacle_list) 
 
 
 def main():
@@ -55,14 +59,12 @@ def main():
     obstacle_sub = rospy.Subscriber("ol1", Ipoly, curr.update_obst_list)
     gp_sub = rospy.Subscriber("global_plan", Float32[], curr.main_response)
     
-    if not target_changed:
-        curr.path = dynamic_rrt(start = curr.curr_pos, end = curr.curr_target, path = curr.path, obstacle_list = curr.obstacle_list)
+    rospy.Timer(rospy.Duration(0.05), curr.dynamic_caller())
 
     path_pub = rospy.Publisher("final_path", Float32[][], queue_size = 10)
     path_pub.publish(curr.path)
 
     rospy.spin()
-
 
 
 if __name__=="__main__":
