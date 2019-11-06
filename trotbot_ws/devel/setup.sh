1e9d3a362b047630abba056620d3586e4f33241d
Harshal Deshpande <hardesh1deshpande@gmail.com>
2019-11-06 23:26:39 +0530

    added gitgnore

---
+++ trotbot_ws/devel/setup.sh
@@ -0,0 +1,1 @@
+/home/harshal/trotbot/trotbot_ws/devel/.private/catkin_tools_prebuild/setup.sh
---
+++ trotbot_ws/devel/share/navigation/cmake/navigationConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/harshal/trotbot/trotbot_ws/devel/.private/navigation/share/navigation/cmake/navigationConfig-version.cmake
