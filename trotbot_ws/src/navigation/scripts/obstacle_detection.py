b197c40845bac85536029a5bd8b3d0a159c385a1
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-11-10 16:35:20 +0530

    Added obstacle detection skeleton

---
+++ trotbot_ws/src/navigation/scripts/obstacle_detection.py
@@ -0,0 +1,78 @@
+import rospy
+import math
+import numpy
+
+from geometry_msgs import Polygon, Point32
+from sensor_msgs.msg import LaserScan
+
+
+#IMPORT CUSTOM MESSAGES HERE
+
+def callback_laserscan(msg):
+	"""
+
+	"""
+	#Split obstacles
+
+	#Expand Obstacles
+
+	#Publish
+
+
+def split_array(ranges, threshold):
+	"""
+	Args: 
+		ranges: Output from laserscan
+		threshold: Threshold for deciding when to start a new obstacle
+
+	Returns: 
+		obstacles_1D: List of list of ranges
+	"""
+
+	return obstacles_1D
+
+def expand(obstacles_1D, expand_dis):
+	"""
+	Args:
+		obstacles_1D: List of lists of ranges in every obstacle
+		expand_dis: Expansion magnitude
+
+	Calls: rtheta_to_xy()
+
+	Returns:
+		obstacles: Array of geometry_msgs.Polygon
+
+	"""
+
+	#For each 1D obstacle, create a new list
+	#Fill the new list by expanding on one side
+	#Append the list by expanding on the other side in reverse order
+
+	#Call rtheta_to_xy for each point in the list
+
+	#Convert new list to geometry_msgs Polygons
+
+	return obstacles
+
+
+def rtheta_to_xy(ranges):
+	"""
+	Args:
+		ranges: list of ranges
+
+	Returns:
+		points: list of geometry_msgs.Point32's
+	"""
+	return points
+
+#-------------------------------
+
+if __name__ == "__main__":
+
+	rospy.init_node("obstacle_detection", anonymous=True)
+
+	#ADD MESSAGE TYPE IN PUBLISHER!!
+	obstacle_pub = rospy.Publisher("obstacles", ,queue_size=5)
+	lidar_sub = rospy.Subscriber("scan", LaserScan, callback_laserscan)
+
+	rospy.spin()
